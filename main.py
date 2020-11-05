343c9760991f18fe778a2b5379e468c9e81cf18e
Lucas Skala <lskala24@biomedscienceacademy.org>
2020-11-05 16:39:05 +0000

    I worked on my story outline and started making input variable

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Lucas Skala
+Period/Core: 2
 
 
 """
+
+print("Welcome to the MadLibs, you will import words to make a story!")
+
+animal = input("What animal do you want? ")
+store = input("What store should we stop at? ")
+item = input("What item do we grab?")
+
+print("the " + animal  + " jumped out of the water, and started sprinting to the " + store "he grabbed a " + item  "and ran without paying. ")
+
+
+
