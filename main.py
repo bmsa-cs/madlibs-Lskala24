073e8d9d6c1c5f0bb7e03823ce8681a36deefb95
Lucas Skala <lskala24@biomedscienceacademy.org>
2020-11-05 16:58:23 +0000

    A few more changes with variables

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ Period/Core: 2
 print("Welcome to the MadLibs, you will import words to make a story!")
 
 animal = input("What animal do you want? ")
-store = input("What store should we stop at? ")
-item = input("What item do we grab?")
+store = input("What store should he stop at? ")
+noun = input("What does he take from the store? ")
+sport = input("What sport does he start to play?  ")
+apparel = input("What does he steal from the police? ")
 
-print("the " + animal  + " jumped out of the water, and started sprinting to the " + store "he grabbed a " + item  "and ran without paying. ")
-
-
-
+print("the " + animal  + " jumped out of the water, and started sprinting to the " + store + " he grabbed some " + noun + " and ran without paying. After everyone was scarmbling he started " + sport + " noone knew what was going on, a quick change was when he started hitting people with the " + noun + ". The animal control was close now, but the " + animal + " had no worries, when they got there he took the " + apparel + " he started beating them with it")
